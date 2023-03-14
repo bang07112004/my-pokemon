bdb60ccbb6c62e352747eaeaecb0b7f01a5ee340
bang07112004 <phong0909191145@gmail.com>
2023-03-14 11:33:35 +0700

    update

--- components/PokeFeed.tsx
+++ components/PokeFeed.tsx
@@ -12,7 +12,7 @@ type Props = {
 function PokeFeed({ name, weight, id, image, type }: Props) {
   return (
     <div
-      className={`flex px-3 py-6 flex-col items-center justify-center border w-fit m-3 rounded-xl ${
+      className={`flex px-3 py-6 flex-col items-center justify-center border w-fit m-3 rounded-xl relative ${
         type[0] === "grass"
           ? "bg-[#76cc54]"
           : type[0] === "fire"
@@ -50,47 +50,8 @@ function PokeFeed({ name, weight, id, image, type }: Props) {
           : "bg-[#ef99ef]"
       }`}
     >
-      <div
-        className={`border-4 ${
-          type[0] === "grass"
-            ? "border-[#76cc54]"
-            : type[0] === "fire"
-            ? "border-[#ff4422]"
-            : type[0] === "water"
-            ? "border-[#3299fe]"
-            : type[0] === "electric"
-            ? "border-[#ffcd32]"
-            : type[0] === "normal"
-            ? "border-[#aaaa99]"
-            : type[0] === "ice"
-            ? "border-[#67cdff]"
-            : type[0] === "fighting"
-            ? "border-[#ba5545]"
-            : type[0] === "poison"
-            ? "border-[#7f3f72]"
-            : type[0] === "ground"
-            ? "border-[#ddba54]"
-            : type[0] === "flying"
-            ? "border-[#8898fe]"
-            : type[0] === "psychic"
-            ? "border-[#ff5599]"
-            : type[0] === "bug"
-            ? "border-[#abba22]"
-            : type[0] === "rock"
-            ? "border-[#baab66]"
-            : type[0] === "ghost"
-            ? "border-[#6667ba]"
-            : type[0] === "dragon"
-            ? "border-[#7766ec]"
-            : type[0] === "dark"
-            ? "border-[#765545]"
-            : type[0] === "steel"
-            ? "border-[#ababbb]"
-            : "border-[#ef99ef]"
-        } `}
-      >
-        <Image src={image} alt={name} width={150} height={150} />
-      </div>
+      <div className="absolute top-2 left-2">{id}</div>
+      <Image src={image} alt={name} width={150} height={150} />
       <div className="flex flex-col items-center justify-center">
         <h1>{name}</h1>
         <p>Weight: {weight}</p>
