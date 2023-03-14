84bc7d9376bb396238d05d5aee128258d011c3cb
bang07112004 <phong0909191145@gmail.com>
2023-03-14 11:17:02 +0700

    update

--- app/pokemon/[pokemonId]/page.tsx
+++ app/pokemon/[pokemonId]/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import getPokemon from "@/libs/getPokemon";
 import PokeFeed from "@/components/PokeFeed";
 
@@ -8,34 +10,53 @@ type Props = {
   };
 };
 
-async function page({ params: { pokemonId } }: Props) {
-  const pokemonData: Promise<Pokemon> = getPokemon(pokemonId);
-  const pokemon = await pokemonData;
-  const abilityNames = pokemon.abilities.map(
-    (ability: { ability: { name: string } }) => ability.ability.name
-  );
-  const moveNames = pokemon.moves.map((move) => move.move.name);
+interface IPokemon {
+  id: string;
+  name: string;
+  weight: string;
+  abilities: { ability: { name: string } }[];
+  moves: { move: { name: string } }[];
+  base_experience: number;
+  sprites: { back_default: string };
+  types: { type: { name: string } }[];
+}
+
+function Page({ params: { pokemonId } }: Props) {
+  const [pokemon, setPokemon] = useState<IPokemon | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const pokemonData = await getPokemon(pokemonId);
+        setPokemon(pokemonData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [pokemonId]);
+
+  if (!pokemon) {
+    return <h1>No Results</h1>;
+  }
+
+  const abilityNames = pokemon.abilities.map(({ ability }) => ability.name);
+  const moveNames = pokemon.moves.map(({ move }) => move.name);
+
   return (
-    <div>
-      {pokemon && pokemon.name.length === 0 && <h1>No Results</h1>}
-      {pokemon && (
-        <div className="flex flex-col justify-center items-center">
-          <PokeFeed
-            name={pokemon.name}
-            weight={pokemon.weight}
-            id={pokemon.id}
-            image={pokemon.sprites.back_default}
-            type={pokemon.types.map(
-              (t: { type: { name: string } }) => t.type.name
-            )}
-          />
-          <h1>Abilities: {abilityNames.join(", ")}</h1>
-          <h1>Base Experience: {pokemon.base_experience}</h1>
-          <h1>Moves: {moveNames.join("/ ")}</h1>
-        </div>
-      )}
+    <div className="flex flex-col justify-center items-center">
+      <PokeFeed
+        name={pokemon.name}
+        weight={pokemon.weight}
+        id={pokemon.id}
+        image={pokemon.sprites.back_default}
+        type={pokemon.types.map(({ type }) => type.name)}
+      />
+      <h1>Abilities: {abilityNames.join(", ")}</h1>
+      <h1>Base Experience: {pokemon.base_experience}</h1>
+      <h1>Moves: {moveNames.join("/ ")}</h1>
     </div>
   );
 }
 
-export default page;
+export default Page;
