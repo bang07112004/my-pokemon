148f29ffda9c67994a4c83b34461ce2aaff5551f
bang07112004 <phong0909191145@gmail.com>
2023-03-15 23:39:38 +0700

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,17 +3,9 @@ import SearchBar from "../components/SearchBar";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="flex flex-col sticky bg-gradient-to-r from-pink-500 to-purple-500 z-50 top-0 py-3 px-2 max-h-[120px]">
+    <div className="flex flex-col sticky max-w-full bg-gradient-to-r from-pink-500 to-purple-500 z-50 top-0 py-3 px-2 max-h-[120px]">
       <div className="flex justify-between items-center">
-        <Link
-          href={{
-            pathname: "/",
-            query: { page: 1 },
-          }}
-          as="/"
-        >
-          Home
-        </Link>
+        <Link href={"/"}>Home</Link>
         <SearchBar />
       </div>
     </div>
